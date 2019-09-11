88bf3fb6f9f9de98d3927d31ca5b7a6754134ef8
mun-su <lms1427@gmail.com>
2019-09-11 23:09:40 +0900

    feat: 회원가입 페이지 기능 추가 회원가입 페이지에 가입시 필요한 input들을 추가하고 각 input이 변경될 경우 리스너를 통하여 리액트 훅을 통해 value값을 변경하도록 만들었습니다. 기본적인 setter기능만 필요한 경우 userInput이라는 커스텀훅을 통하여 처리 하였고 그 외 경우에는 핸들러에서 처리한 후 훅의 setter을 호출하는 형식으로 구현하였습니다.

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import AppLayout from "./components/AppLayout";
+import { Form, Input, Checkbox, Button } from "antd";
 
 const Sinup = () => {
+  const userInput = (initValue = null, afterHadler = undefined) => {
+    const [value, setter] = useState(initValue);
+    const handler = e => {
+      setter(e.target.value);
+      if (afterHadler) afterHadler(e);
+    };
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = userInput("");
+  const [nick, onChangeNick] = userInput("");
+  const [password, onChangePassword] = userInput("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [term, setTerm] = useState(false);
+  const [passwrodError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password != passwordCheck) setPasswordError(true);
+    if (!term) setTermError(true);
+    console.log({
+      id,
+      nick,
+      password,
+      passwordCheck,
+      term
+    });
+  };
+
+  const onChangePasswordCheck = e => {
+    passwordErrorHandler(password == e.target.value);
+    setPasswordCheck(e.target.value);
+  };
+
+  const onChangeTerm = e => {
+    setTermError(false);
+    setTerm(!term);
+  };
+
   return (
     <>
       <Head>
@@ -13,7 +54,61 @@ const Sinup = () => {
         />
       </Head>
       <AppLayout>
-        <div>회원가입</div>
+        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nick">닉네임</label>
+            <br />
+            <Input
+              name="user-nick"
+              value={nick}
+              required
+              onChange={onChangeNick}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input
+              name="user-password"
+              type="password"
+              value={password}
+              required
+              onChange={onChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호체크</label>
+            <br />
+            <Input
+              name="user-password-check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onChangePasswordCheck}
+            />
+            {passwrodError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              해당 약관에 동의합니다.
+            </Checkbox>
+            {termError && (
+              <div style={{ color: "red" }}>약관에 동의해주세요.</div>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
       </AppLayout>
     </>
   );
