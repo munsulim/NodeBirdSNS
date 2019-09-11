b372a05393adf56b9535f68574537d8ef1ae3eaf
mun-su <lms1427@gmail.com>
2019-09-12 00:39:09 +0900

    improvement: 페이지의 중복 컴포넌트 분리
    페이지마다 중복되는 컴포넌트를 _app.js로 옮기고 다른 페이지들은 app.js에서
    컴포넌트로 받아 작동하여 각페이지 내부에서 리렌더링이 될 경우 중복되는 컴포넌트는
    리렌더링이 되지 않도록 최적화함.

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import Link from "next/link";
-import Head from "next/head";
-import AppLayout from "./components/AppLayout";
 
 const Home = () => {
-    return (
-        <>
-            <Head>
-                <title>NodeBird</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.2/antd.css"/>
-            </Head>
-            <AppLayout>
-                <div>Hello, Next!</div>
-            </AppLayout>
-        </>
-    );
+  return (
+    <>
+      <div>Hello, Next!</div>
+    </>
+  );
 };
 
 export default Home;
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,6 +1,4 @@
 import React, { useState, useCallback } from "react";
-import Head from "next/head";
-import AppLayout from "./components/AppLayout";
 import { Form, Input, Checkbox, Button } from "antd";
 
 const Sinup = () => {
@@ -47,70 +45,61 @@ const Sinup = () => {
 
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.2/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
-          <div>
-            <label htmlFor="user-id">아이디</label>
-            <br />
-            <Input name="user-id" value={id} required onChange={onChangeId} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">닉네임</label>
-            <br />
-            <Input
-              name="user-nick"
-              value={nick}
-              required
-              onChange={onChangeNick}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input
-              name="user-password"
-              type="password"
-              value={password}
-              required
-              onChange={onChangePassword}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password-check">비밀번호체크</label>
-            <br />
-            <Input
-              name="user-password-check"
-              type="password"
-              value={passwordCheck}
-              required
-              onChange={onChangePasswordCheck}
-            />
-            {passwrodError && (
-              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-              해당 약관에 동의합니다.
-            </Checkbox>
-            {termError && (
-              <div style={{ color: "red" }}>약관에 동의해주세요.</div>
-            )}
-          </div>
-          <div style={{ marginTop: 10 }}>
-            <Button type="primary" htmlType="submit">
-              가입하기
-            </Button>
-          </div>
-        </Form>
-      </AppLayout>
+      <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nick}
+            required
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwrodError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            해당 약관에 동의합니다.
+          </Checkbox>
+          {termError && (
+            <div style={{ color: "red" }}>약관에 동의해주세요.</div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
     </>
   );
 };
