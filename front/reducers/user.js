4775ad9bd6eaa0dc496131ea3436d007db882364
mun-su <lms1427@gmail.com>
2019-09-12 17:58:02 +0900

    feat: 리덕스를 추가하여 리듀서 생성
    로그인, 로그아웃 액션을 담은 user 리듀서와 포스트를 추가하는 액션을 담은 post 리듀서를 만들고
    이를 index 리듀서에서 콤바인 리덕스를 이용해 결합하였음.

---
+++ front/reducers/user.js
@@ -0,0 +1,44 @@
+export const initialState = {
+  isLoggedIn: false,
+  user: {}
+};
+
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+
+export const loginAction = {
+  type: LOG_IN,
+  data: {
+    nickname: "보르바"
+  }
+};
+
+export const logoutAction = {
+  type: LOG_OUT
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOG_IN: {
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.data
+      };
+    }
+    case LOG_OUT: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null
+      };
+    }
+    default: {
+      return {
+        state
+      };
+    }
+  }
+};
+
+export default reducer;
