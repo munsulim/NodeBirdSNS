902475362b659eba6483efdebaa74a0034ad11bc
munsulim <lms1427@gmail.com>
2019-10-11 23:36:09 +0900

    feat: 회원가입 API 구현
    프론트에서 요청한 정보로 아이디 중복확인 후 비밀번호
    를 bcrypt를 통하여 처리하고 데이터베이스에 추가하도
    록 구현함.

--- back/index.js
+++ back/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api/user', (req, res) => {});
+app.post('/api/user', (req, res) => {});
+
+app.get('/api/user/:id', (req, res) => {});
+
+app.post('/api/user/logout', (req, res) => {});
+
+app.post('/api/user/login', (req, res) => {});
+
 app.get('/', (res, req, next) => {});
 
 app.listen(3065, () => {
