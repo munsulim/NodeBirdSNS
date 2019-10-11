865e240a7d3c1227bcb89ea0f58d0ff56d46f09c
munsulim <lms1427@gmail.com>
2019-10-12 00:47:02 +0900

    chore: 비밀번호 관리를 위해 dotenv 추가
    비밀번호 관리를 위해 dotenv를 추가하고 해당 파일이
    어떤 항목을 가지고 있는지 devenv로 공개하였음.
    dotenv를 사용하기위해 config를 json에서 js로 변경

---
+++ back/config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+  }
+};
